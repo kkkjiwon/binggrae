82c5f37ac1658ee8caad015689afe40e507b9e7a
jiwon <wldnjs5391@gmail.com>
2024-05-21 15:01:03 +0900

    js 파일 분리

---
+++ js/brand.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+var brandSwiper = new Swiper(".brandSwiper", {
+    slidesPerView: 6,
+    spaceBetween: 20,
+    loop: false, // 슬라이드 반복 여부
+    // navigation
+    navigation: {
+      nextEl: ".brandshop .next",
+      prevEl: ".brandshop .prev",
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        grid: {
+          rows: 3,
+          fill: "row",
+        },
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+        // grid: {
+        //     rows: 2,
+        //     fill: "row",
+        //   },
+      },
+      1200: {
+        slidesPerView: 5,
+        spaceBetween: 10,
+      },
+      1400: {
+        slidesPerView: 6,
+        spaceBetween: 10,
+      },
+    },
+  });
+});
